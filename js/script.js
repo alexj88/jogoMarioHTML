4b9e9383c914ce35027c1b5f29752a3f6aaa8159
Alex <a.johny87@gmail.com>
2025-03-24 21:34:00 -0300

    new commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const mario = document.querySelector('.mario')
-
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 const jump = () => {
     mario.classList.add('jump');
 
@@ -9,7 +9,21 @@ const jump = () => {
 
 }
 
+const loop =setInterval(() => {
 
+const pipePosition = pipe.offsetLeft;
+const marioPosition = window.getComputedStyle(mario).bottom;
+
+console.log(marioPosition);
+
+if (pipePosition <= 120) { 
+
+pipe.style.animation = 'none';
+pipe.style.left = `${pipePosition}px`;
+
+}
+
+}, 10);
 
 
 document.addEventListener('keydown', jump); 

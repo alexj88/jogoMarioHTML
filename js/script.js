03bbcc01a1498be13591f46b120a6d8d6403fa50
Alex <a.johny87@gmail.com>
2025-03-29 18:05:12 -0300

    concluido

--- js/script.js
+++ js/script.js
@@ -11,15 +11,26 @@ const jump = () => {
 
 const loop =setInterval(() => {
 
+    console.log('loop')
+
 const pipePosition = pipe.offsetLeft;
 const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ' ');
 
 console.log(marioPosition);
     
-if (pipePosition <= 120) {
+if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
 pipe.style.animation = 'none';
 pipe.style.left = `${pipePosition}px`;
+
+mario.style.animation = 'none';
+mario.style.bottom = `${marioPosition}px`;
+
+mario.src = './images/game-over.png';
+mario.style.width = '75px';
+mario.style.margin = '50px';
+
+clearInterval(loop);
 
 }           
 
